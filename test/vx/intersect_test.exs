9da2d322bbb0e675f08a6b78fe2a839b8de7e4b6
Tobias <tobias.casper@gmail.com>
2024-01-14 16:35:53 +0100

    update intersect and union tests

--- test/vx/intersect_test.exs
+++ test/vx/intersect_test.exs
@@ -3,25 +3,31 @@ defmodule Vx.IntersectTest do
 
   describe "t/1" do
     test "match" do
-      assert Vx.valid?(Vx.intersect([Vx.String.t()]), "foo")
+      assert :ok = Vx.validate(Vx.intersect([Vx.String.t()]), "foo")
 
-      assert Vx.valid?(Vx.intersect([Vx.String.t(), "foo"]), "foo")
+      assert :ok = Vx.validate(Vx.intersect([Vx.String.t(), "foo"]), "foo")
 
-      assert Vx.valid?(
-               Vx.intersect([Vx.String.min_length(2), Vx.String.max_length(4)]),
-               "foo"
-             )
+      assert :ok =
+               Vx.validate(
+                 Vx.intersect([Vx.String.min_length(2), Vx.String.max_length(4)]),
+                 "foo"
+               )
     end
 
     test "no match" do
-      refute Vx.valid?(Vx.intersect([Vx.Integer.t()]), "foo")
-      refute Vx.valid?(Vx.intersect([Vx.String.t(), Vx.Number.t()]), "foo")
-      refute Vx.valid?(Vx.intersect([Vx.String.t(), "bar"]), "foo")
+      assert {:error, _} = Vx.validate(Vx.intersect([Vx.Integer.t()]), "foo")
 
-      refute Vx.valid?(
-               Vx.intersect([Vx.String.min_length(1), Vx.String.max_length(2)]),
-               "foo"
-             )
+      assert {:error, _} =
+               Vx.validate(Vx.intersect([Vx.String.t(), Vx.Number.t()]), "foo")
+
+      assert {:error, _} =
+               Vx.validate(Vx.intersect([Vx.String.t(), "bar"]), "foo")
+
+      assert {:error, _} =
+               Vx.validate(
+                 Vx.intersect([Vx.String.min_length(1), Vx.String.max_length(2)]),
+                 "foo"
+               )
     end
   end
 end
--- test/vx/union_test.exs
+++ test/vx/union_test.exs
@@ -3,32 +3,35 @@ defmodule Vx.UnionTest do
 
   describe "t/1" do
     test "match" do
-      assert Vx.valid?(Vx.union([Vx.String.t()]), "foo")
-      assert Vx.valid?(Vx.union([Vx.String.t(), Vx.Integer.t()]), "foo")
+      assert :ok = Vx.validate(Vx.union([Vx.String.t()]), "foo")
+      assert :ok = Vx.validate(Vx.union([Vx.String.t(), Vx.Integer.t()]), "foo")
 
-      assert Vx.valid?(
-               Vx.union([
-                 Vx.String.min_length(6),
-                 Vx.String.min_length(3),
-                 Vx.Integer.t()
-               ]),
-               "foo"
-             )
+      assert :ok =
+               Vx.validate(
+                 Vx.union([
+                   Vx.String.min_length(6),
+                   Vx.String.min_length(3),
+                   Vx.Integer.t()
+                 ]),
+                 "foo"
+               )
     end
 
     test "no match" do
-      refute Vx.valid?(Vx.union([Vx.Integer.t()]), "foo")
-      refute Vx.valid?(Vx.union(["bar", Vx.Integer.t()]), "foo")
+      assert {:error, _} = Vx.validate(Vx.union([Vx.Integer.t()]), "foo")
+      assert {:error, _} = Vx.validate(Vx.union(["bar", Vx.Integer.t()]), "foo")
 
-      refute Vx.valid?(
-               Vx.union([Vx.String.min_length(4), Vx.Integer.t()]),
-               "foo"
-             )
+      assert {:error, _} =
+               Vx.validate(
+                 Vx.union([Vx.String.min_length(4), Vx.Integer.t()]),
+                 "foo"
+               )
 
-      refute Vx.valid?(
-               Vx.union([Vx.String.min_length(4), Vx.Integer.t()]),
-               "foo"
-             )
+      assert {:error, _} =
+               Vx.validate(
+                 Vx.union([Vx.String.min_length(4), Vx.Integer.t()]),
+                 "foo"
+               )
     end
   end
 end
