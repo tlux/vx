a44aaa64ca2b38fc54dc1455e70466635d724f2c
Tobias Casper <tobias.casper@gmail.com>
2024-01-16 02:05:07 +0100

    add atom test

---
+++ test/vx/atom_test.exs
@@ -0,0 +1,17 @@
+defmodule Vx.AtomTest do
+  use ExUnit.Case, async: true
+
+  describe "t/0" do
+    test "match" do
+      Enum.each([nil, :foo, true, false], fn value ->
+        assert :ok = Vx.validate(Vx.Atom.t(), value)
+      end)
+    end
+
+    test "no match" do
+      Enum.each([123, 123.4, "foo", %{}, []], fn value ->
+        assert {:error, _} = Vx.validate(Vx.Atom.t(), value)
+      end)
+    end
+  end
+end
