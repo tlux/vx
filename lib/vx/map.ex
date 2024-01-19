444aa3b405fe4ffee3611df653eae805ffa834d2
Tobias Casper <tobias.casper@gmail.com>
2024-01-20 00:33:40 +0100

    add match_mode

--- lib/vx/map.ex
+++ lib/vx/map.ex
@@ -55,7 +55,7 @@ defmodule Vx.Map do
       type,
       :partial,
       &check_partial(&1, shape),
-      %{shape: shape, match_mode: :partial},
+      %{shape: shape},
       "does not have the expected partial shape"
     )
   end
@@ -116,7 +116,7 @@ defmodule Vx.Map do
       type,
       :shape,
       &check_shape(&1, shape),
-      %{shape: shape, match_mode: :exact},
+      %{shape: shape},
       "does not have the expected shape"
     )
   end
