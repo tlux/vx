b834d53ee41b3697450d6886757c1ebeca368f0e
Tobias Casper <tobias.casper@gmail.com>
2024-01-28 19:54:50 +0100

    remove any protocol

--- lib/vx/validatable.ex
+++ lib/vx/validatable.ex
@@ -3,19 +3,9 @@ defprotocol Vx.Validatable do
   The Validatable protocol must be implemented by all types validatable by Vx.
   """
 
-  @fallback_to_any true
-
   @doc """
   Validates whether a value matches agianst the given type or schema.
   """
   @spec validate(t, any) :: :ok | {:error, Vx.ValidationError.t()}
   def validate(input, value)
-end
-
-defimpl Vx.Validatable, for: Any do
-  def validate(input, value) do
-    input
-    |> Vx.eq()
-    |> Vx.Validatable.validate(value)
-  end
 end
