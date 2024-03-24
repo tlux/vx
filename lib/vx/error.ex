cd2cd8fa7fb9e2efa042c697260fc5bcab0b1cab
Tobias Casper <tobias.casper@gmail.com>
2024-03-24 21:35:52 +0100

    docs: add moduledoc for Vx.Error type

--- lib/vx/error.ex
+++ lib/vx/error.ex
@@ -1,4 +1,8 @@
 defmodule Vx.Error do
+  @moduledoc """
+  An error that is returned or raised when validation fails.
+  """
+
   @enforce_keys [:schema, :value, :message]
   defexception [:schema, :value, :message]
 
